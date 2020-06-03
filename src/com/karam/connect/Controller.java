92d185572cc95168dd744c9d1103b850830dccd3
karammm <karamjitverma9@gmail.com>
2020-06-03 13:04:01 +0530

    Animation

--- src/com/karam/connect/Controller.java
+++ src/com/karam/connect/Controller.java
@@ -1,10 +1,13 @@
 package com.karam.connect;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 
 import java.net.URL;
@@ -15,16 +18,29 @@ public class Controller implements Initializable {
 	@FXML
 	public Pane myPane;
 
+	@FXML
+	public Button myButton;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
 		Circle circle=new Circle();
 		circle.setRadius(20);
 		circle.setFill(Color.RED);
-		circle.setTranslateX(50);
-		circle.setTranslateY(50);
+		circle.setTranslateX(50); //Move the circe 50 pixel along the positive x axis.
+		circle.setTranslateY(50); //Move the circe 50 pixel along the positive y axis.
 
 
 		myPane.getChildren().add(circle);
+
+		myButton.setOnAction(event -> {
+			startAnimation(circle);
+		});
+	}
+
+	private void startAnimation(Circle circle) {
+		TranslateTransition transition=new TranslateTransition(Duration.seconds(.5),circle);//parametrised constructor
+		transition.setToY(200);
+		transition.play();
 	}
 }
